82e4d87fe35603d8c9d2a85b16680ac919415fad
nevillco <connor.neville16@gmail.com>
2020-03-25 14:04:49 -0400

    Add some extra packages to Package.swift for future Auth purposes.

--- Package.swift
+++ Package.swift
@@ -7,11 +7,10 @@ let package = Package(
         .library(name: "SettleBoard-API", targets: ["App"]),
     ],
     dependencies: [
-        // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
+        .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0"),
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
