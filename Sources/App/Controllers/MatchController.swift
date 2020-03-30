a171a1c6895812f7943e653b7d99b50a3ae82649
nevillco <connor.neville16@gmail.com>
2020-03-29 22:07:03 -0400

    Ensure errors in finding UUIDs prevent creation of Match models.

--- Sources/App/Controllers/MatchController.swift
+++ Sources/App/Controllers/MatchController.swift
@@ -14,27 +14,30 @@ extension MatchController {
     func create(_ request: Request, _ input: CreateMatchInput) throws -> Future<[Score]> {
         return validate(input, on: request)
             .flatMap { self.fetchUserValues(for: $0, on: request) }
-            .and(Match().create(on: request).map { try $0.requireID() })
-            .flatMap { (tuple1: ([FetchedValue], Match.ID)) -> Future<[(User, Score)]> in
-                let (userValues, matchID) = tuple1
-                return request.eventLoop.future(userValues)
-                    .flatMap { userValues -> Future<[(User, Score)]> in
-                        return userValues.map { tuple -> Future<(User, Score)> in
-                            let (user, userID, value) = tuple
-                            let isVictory = value >= 10
-                            user.wins += (isVictory ? 1 : 0)
-                            user.gamesPlayed += 1
-                            user.points += value
-                            let score = Score(
-                                value: value,
-                                isVictory: isVictory,
-                                userID: userID,
-                                matchID: matchID)
-                            return user.update(on: request)
-                                .and(score.create(on: request))
-                        }
-                        .flatten(on: request)
-                }
+            .flatMap { fetchedValues in
+                return Match().create(on: request)
+                    .map { (fetchedValues, try $0.requireID()) }
+        }
+        .flatMap { tuple -> Future<[(User, Score)]> in
+            let (userValues, matchID) = tuple
+            return request.eventLoop.future(userValues)
+                .flatMap { userValues -> Future<[(User, Score)]> in
+                    return userValues.map { tuple -> Future<(User, Score)> in
+                        let (user, userID, value) = tuple
+                        let isVictory = value >= 10
+                        user.wins += (isVictory ? 1 : 0)
+                        user.gamesPlayed += 1
+                        user.points += value
+                        let score = Score(
+                            value: value,
+                            isVictory: isVictory,
+                            userID: userID,
+                            matchID: matchID)
+                        return user.update(on: request)
+                            .and(score.create(on: request))
+                    }
+                    .flatten(on: request)
+            }
         }
         .map { $0.map { $0.1 } }
     }
@@ -79,7 +82,7 @@ extension MatchController {
                 errorReason: "Detected some number of scores other than 3 or 4.")
             .validate(
                 input.scores.filter { $0.value >= 10 }.count == 1,
-                errorReason: "Detected more than 1 score over 10.")
+                errorReason: "Exactly 1 specified score must be over 10.")
             .validate(
                 input.scores.allSatisfy { $0.value >= 2 },
                 errorReason: "All scores should be greater than 2.")
@@ -93,9 +96,9 @@ extension MatchController {
             let (userIDString, value) = tuple
             return worker.eventLoop.newSucceededFuture(result: userIDString)
                 .map(UUID.init(uuidString:))
-                .unwrap(or: Abort(.badRequest))
+                .unwrap(or: Abort(.badRequest, reason: "An invalid UUID was passed."))
                 .flatMap { User.find($0, on: worker) }
-                .unwrap(or: Abort(.badRequest))
+                .unwrap(or: Abort(.badRequest, reason: "No user was found for one of the given IDs."))
                 .map { ($0, try $0.requireID(), value) }
         }
         .flatten(on: worker)
